388fbed6712f1e6f40fc2558a4c2f35c182393c8
grv <gaurav083076@gmail.com>
2026-01-11 22:16:17 +0530

    Routes seperated : category and trip

---
+++ src/routes/category.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const  categoryRouter = express.Router();
+const categoryModel = require("../models/category");
+
+
+categoryRouter.post("/categories",async (req,res) => {
+    try {
+        const categoryName = req.body;
+        const addCategory = new categoryModel(categoryName);
+        await addCategory.save();
+        res.status(201).send("Category added successfully.")
+    }
+    catch(err)
+    {
+        if (err.name === "ValidationError")
+            res.status(400).send("Error: " + err.message);
+        else
+            res.status(500).send("Error: " + err.message);
+    }
+})
+
+categoryRouter.get("/categories",async (req,res) => {
+    try {
+        const categories = await categoryModel.find();
+        res.status(200).send(
+            {categories: categories}
+        );
+    }
+    catch(err)
+    {
+        res.status(500).send("Error: "+err);
+    }
+})
+
+module.exports = categoryRouter;
---
+++ src/routes/trip.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const tripRouter = express.Router();
+const tripModel = require("../models/trip");
+const categoryModel = require("../models/category")
+
+tripRouter.post("/trips",async (req,res) => {
+    try {
+        const tripDetails = req.body;
+        const addTrip = new tripModel(tripDetails);
+
+        if (tripDetails.categories && tripDetails.categories.length > 0) {
+            const validCategories = await categoryModel.find({
+            _id: { $in: tripDetails.categories }
+            });
+        if (validCategories.length !== tripDetails.categories.length) {
+            return res.status(400).send({ message: "Invalid category ID" });
+            }
+        }        
+        await addTrip.save();
+        res.status(201).send({
+            "message":"Trip added successfully",
+            "data":addTrip
+        })
+    }
+    catch(err)
+    {
+        if (err.name === "ValidationError")
+            res.status(400).send("Error: "+err.message)
+        else
+            res.status(500).send("Error: "+err.message);
+    }
+})
+
+tripRouter.post("/trips/:id/book",async (req,res) => {
+    try{
+    const _id = req.params.id;
+    const trip = await tripModel.findById(_id);
+
+    if (!trip)
+        return res.status(404).send({message:"Trip not found"});
+
+    if (trip.availableSeat === 0)
+        return res.status(400).send({message:"No seat available"});
+
+    trip.availableSeat = trip.availableSeat - 1;
+    await trip.save();
+    res.status(200).send({
+        message:"Booking successful",
+        availableSeat:trip.availableSeat
+    })
+  }
+  catch(err)
+  {
+    if (err.name === "CastError")
+        res.status(400).send({ message: "Invalid trip ID" });
+    else
+        res.status(500).send("Error: "+err.message);
+  }
+})
+
+tripRouter.get("/trips",async (req,res) => {
+    try {
+        const { destination,startDate,category,minPrice,maxPrice,sortBy,order } = req.query; 
+        const filter = {status:'Published'};
+        if (destination)
+        {
+            filter.destination = destination;
+        } 
+        if (startDate)
+        {
+            filter.startDate = { $gte: new Date(startDate) };
+        }
+        if (category) 
+        {
+            filter.categories = category;
+        }
+        if (minPrice || maxPrice)
+        {
+            filter.price = {};
+            if (minPrice) filter.price.$gte = Number(minPrice);
+            if (maxPrice) filter.price.$lte = Number(maxPrice);
+        }
+        const allowedSortFields = ["startDate","price"];
+        let sort = {};
+        if (allowedSortFields.includes(sortBy))
+        {
+            sort[sortBy] = order === "desc" ? -1 : 1;
+        }
+        const trips = await tripModel.find(filter).sort(sort);
+        res.status(200).send({trips});
+    }
+    catch(err)
+    {
+        res.status(500).send("Error: "+err.message);
+    }
+})
+
+tripRouter.get("/trips/:id",async (req,res) => {
+    try{
+        const _id = req.params.id;
+        const tripDetails = await tripModel.findById(_id).populate("categories","name -_id");
+        if (!tripDetails)
+        {
+            return res.status(404).send({ message: "No trip found" });
+        }
+        res.status(200).send(tripDetails);
+    }
+    catch(err)
+    {
+        if (err.name === "CastError")
+            res.status(400).send({ message: "Invalid trip ID" });
+        else
+        res.status(500).send("Error: "+err.message);
+    }
+})
+
+module.exports = tripRouter;
